8b7eeb8e7c35f033ce399f4031ee1ed4981c3a73
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-08-17 15:17:43 +0100

    DAC6-1932: Add send your file page

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -58,4 +58,6 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
 
   val cacheTtl: Int = configuration.get[Int]("mongodb.timeToLiveInSeconds")
+
+  val spinnerCounter: Int = configuration.get[Int]("spinner.counter")
 }
