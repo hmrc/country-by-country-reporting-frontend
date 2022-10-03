2fc155b29716962b2062fad5e502930a75ef9dfc
harryhamilton <8810902+HarryHamilton@users.noreply.github.com>
2022-10-03 09:44:13 +0100

    DAC6-2019: Created agent second contact page

--- test-utils/generators/PageGenerators.scala
+++ test-utils/generators/PageGenerators.scala
@@ -21,6 +21,9 @@ import pages._
 
 trait PageGenerators {
 
+  implicit lazy val arbitraryAgentSecondContactPage: Arbitrary[AgentSecondContactPage.type] =
+    Arbitrary(AgentSecondContactPage)
+
   implicit lazy val arbitraryFirstContactNamePage: Arbitrary[AgentFirstContactNamePage.type] =
     Arbitrary(AgentFirstContactNamePage)
 
--- test-utils/generators/UserAnswersGenerator.scala
+++ test-utils/generators/UserAnswersGenerator.scala
@@ -27,7 +27,8 @@ trait UserAnswersGenerator extends TryValues {
   self: Generators =>
 
   val generators: Seq[Gen[(QuestionPage[_], JsValue)]] =
-    arbitrary[(AgentFirstContactNamePage.type, JsValue)] ::
+    arbitrary[(AgentSecondContactPage.type, JsValue)] ::
+      arbitrary[(AgentFirstContactNamePage.type, JsValue)] ::
       arbitrary[(SecondContactEmailPage.type, JsValue)] ::
       arbitrary[(ContactNamePage.type, JsValue)] ::
       arbitrary[(HaveTelephonePage.type, JsValue)] ::
