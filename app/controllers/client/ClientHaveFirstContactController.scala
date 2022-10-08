261c69a88e2a0df125b6c606526236b12100a16a
Shailesh <shaileshp@gmail.com>
2022-10-08 08:39:13 +0200

    DAC6-2038 - Added Have first contact

---
+++ app/controllers/client/ClientHaveFirstContactController.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.client
+
+import controllers.actions._
+import forms.HaveFirstContactFormProvider
+import models.{CheckMode, Mode}
+import navigation.ContactDetailsNavigator
+import pages.HaveSecondContactPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.ContactHelper
+import views.html.client.ClientHaveFirstContactView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class ClientHaveFirstContactController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: ContactDetailsNavigator,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  formProvider: HaveFirstContactFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: ClientHaveFirstContactView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport
+    with ContactHelper {
+
+  val form = formProvider("clientHaveFirstContact")
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData() andThen requireData) {
+    implicit request =>
+      val preparedForm = request.userAnswers.get(HaveSecondContactPage) match {
+        case Some(value) => form.fill(value)
+        case None        => form
+      }
+
+      Ok(view(preparedForm, mode, getFirstContactName(request.userAnswers)))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData() andThen requireData).async {
+    implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, getFirstContactName(request.userAnswers)))),
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(HaveSecondContactPage, value))
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(HaveSecondContactPage, CheckMode, updatedAnswers))
+        )
+  }
+}
---
+++ test/controllers/client/ClientHaveFirstContactControllerSpec.scala
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.client
+
+import base.SpecBase
+import forms.{HaveFirstContactFormProvider, HaveSecondContactFormProvider}
+import models.{NormalMode, UserAnswers}
+import navigation.{ContactDetailsNavigator, FakeContactDetailsNavigator}
+import org.mockito.ArgumentMatchers.any
+import org.scalatestplus.mockito.MockitoSugar
+import pages.{ContactNamePage, HaveSecondContactPage}
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import views.html.client.ClientHaveFirstContactView
+
+import scala.concurrent.Future
+
+class ClientHaveFirstContactControllerSpec extends SpecBase with MockitoSugar {
+
+  override def onwardRoute = Call("GET", "/foo")
+
+  val formProvider = new HaveFirstContactFormProvider()
+  val form         = formProvider("clientHaveFirstContact")
+
+  private val name = "First Contact Name"
+
+  lazy val clientHaveFirstContactRoute = routes.ClientHaveFirstContactController.onPageLoad(NormalMode).url
+
+  "ClientHaveSecondContact Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val userAnswers = emptyUserAnswers.set(ContactNamePage, name).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, clientHaveFirstContactRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[ClientHaveFirstContactView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode, name)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId)
+        .set(ContactNamePage, name)
+        .success
+        .value
+        .set(HaveSecondContactPage, true)
+        .success
+        .value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, clientHaveFirstContactRoute)
+
+        val view = application.injector.instanceOf[ClientHaveFirstContactView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(true), NormalMode, name)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val userAnswers = emptyUserAnswers.set(ContactNamePage, name).success.value
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswers))
+          .overrides(
+            bind[ContactDetailsNavigator].toInstance(new FakeContactDetailsNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, clientHaveFirstContactRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val userAnswers = emptyUserAnswers.set(ContactNamePage, name).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, clientHaveFirstContactRoute)
+            .withFormUrlEncodedBody(("value", ""))
+
+        val boundForm = form.bind(Map("value" -> ""))
+
+        val view = application.injector.instanceOf[ClientHaveFirstContactView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode, name)(request, messages(application)).toString
+      }
+    }
+
+  }
+}
