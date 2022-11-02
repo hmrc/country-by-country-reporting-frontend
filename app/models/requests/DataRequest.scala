07dfa9b0795cb1f111c5904783721bda26c2381b
harryhamilton <8810902+HarryHamilton@users.noreply.github.com>
2022-11-02 08:04:50 +0000

    DAC6-2094: Create isIndividual helper function

--- app/models/requests/DataRequest.scala
+++ app/models/requests/DataRequest.scala
@@ -19,15 +19,17 @@ package models.requests
 import play.api.mvc.{Request, WrappedRequest}
 import models.UserAnswers
 import uk.gov.hmrc.auth.core.AffinityGroup
-import uk.gov.hmrc.auth.core.AffinityGroup.Agent
+import uk.gov.hmrc.auth.core.AffinityGroup.{Agent, Individual}
 
 case class OptionalDataRequest[A](request: Request[A], userId: String, userAnswers: Option[UserAnswers], userType: AffinityGroup, subscriptionId: String)
     extends WrappedRequest[A](request) {
-  def isAgent = userType == Agent
+  def isAgent      = userType == Agent
+  def isIndividual = userType == Individual
 }
 
 case class DataRequest[A](request: Request[A], userId: String, subscriptionId: String, userType: AffinityGroup, userAnswers: UserAnswers)
     extends WrappedRequest[A](request) {
 
-  def isAgent = userType == Agent
+  def isAgent      = userType == Agent
+  def isIndividual = userType == Individual
 }
