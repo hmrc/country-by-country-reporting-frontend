203090acfcf46f46485d47ff20176862ac85b85f
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-11-14 15:01:24 +0000

    DAC6-2142: Move main template to use hmrcLayout instead of govUkLayout

--- test/controllers/actions/FakeAgentIdentifierAction.scala
+++ test/controllers/actions/FakeAgentIdentifierAction.scala
@@ -18,7 +18,7 @@ package controllers.actions
 
 import models.requests.IdentifierRequest
 import play.api.mvc._
-import uk.gov.hmrc.auth.core.AffinityGroup.{Agent, Organisation}
+import uk.gov.hmrc.auth.core.AffinityGroup.Agent
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
