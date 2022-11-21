aed46f267d4c000c3b4f89914f44b74e4f190977
harryhamilton <8810902+HarryHamilton@users.noreply.github.com>
2022-11-21 14:56:41 +0000

    DAC6-2157: fix controller and imports

--- app/controllers/agent/AgentIsThisYourClientController.scala
+++ app/controllers/agent/AgentIsThisYourClientController.scala
@@ -9,7 +9,7 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.AgentIsThisYourClientView
+import views.html.agent.AgentIsThisYourClientView
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
