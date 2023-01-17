32e6fbd5596a1eae3ab8cdf4da9f8c06a523eb24
harryhamilton <8810902+HarryHamilton@users.noreply.github.com>
2023-01-17 12:46:30 +0000

    DAC6-2234: Update imports in ProblemCBCIdControllerSpec

--- test/controllers/client/ProblemCBCIdControllerSpec.scala
+++ test/controllers/client/ProblemCBCIdControllerSpec.scala
@@ -19,7 +19,7 @@ package controllers.client
 import base.SpecBase
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import views.html.client.problemCBCIdView
+import views.html.client.ProblemCBCIdView
 
 class ProblemCBCIdControllerSpec extends SpecBase {
 
@@ -34,7 +34,7 @@ class ProblemCBCIdControllerSpec extends SpecBase {
 
         val result = route(application, request).value
 
-        val view = application.injector.instanceOf[problemCBCIdView]
+        val view = application.injector.instanceOf[ProblemCBCIdView]
 
         status(result) mustEqual OK
         contentAsString(result) mustEqual view()(request, messages(application)).toString
