371802ce79d9b30ecc49b6cdea7ca20e435c3674
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2023-02-06 09:21:05 +0000

    DAC6-2287: Address PR comments

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -19,12 +19,14 @@ package controllers
 import connectors.FileDetailsConnector
 import controllers.actions.{DataRetrievalAction, IdentifierAction}
 import models.UserAnswers
+import models.requests.OptionalDataRequest
 import pages.{AgentFirstContactNamePage, ContactNamePage, JourneyInProgressPage}
 import play.api.Logging
-import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Request, Result}
 import repositories.SessionRepository
 import services.{AgentSubscriptionService, SubscriptionService}
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.IndexView
 
@@ -47,57 +49,71 @@ class IndexController @Inject() (
 
   def onPageLoad: Action[AnyContent] = (identify andThen getData.apply) async {
     implicit request =>
-      Future.fromTry(request.userAnswers.getOrElse(UserAnswers(request.userId)).set(JourneyInProgressPage, true)).flatMap {
+      setContactDetailsFlag(request.userAnswers.getOrElse(UserAnswers(request.userId))).flatMap {
         ua =>
-          sessionRepository.set(ua).flatMap {
-            _ =>
-              if (request.isAgent) {
-                agentSubscriptionService.getAgentContactDetails(ua) flatMap {
-                  agentContactDetails =>
-                    subscriptionService.getContactDetails(agentContactDetails.getOrElse(ua), request.subscriptionId) flatMap {
-                      clientContactDetails =>
-                        (agentContactDetails, clientContactDetails) match {
-                          case (Some(agentUserAnswers), Some(clientUserAnswers)) =>
-                            sessionRepository.set(agentUserAnswers).flatMap {
-                              _ =>
-                                sessionRepository.set(clientUserAnswers).flatMap {
-                                  _ =>
-                                    if (agentUserAnswers.get(AgentFirstContactNamePage).isEmpty) {
-                                      Future.successful(Redirect(controllers.agent.routes.AgentContactDetailsNeededController.onPageLoad()))
-                                    } else if (clientUserAnswers.get(ContactNamePage).isEmpty) {
-                                      Future.successful(Redirect(controllers.client.routes.ClientContactDetailsNeededController.onPageLoad()))
-                                    } else {
-                                      fileConnector.getAllFileDetails(request.subscriptionId) map {
-                                        fileDetails =>
-                                          Ok(view(fileDetails.isDefined, request.subscriptionId, request.isAgent))
-                                      }
-                                    }
-                                }
-                            }
-                          case _ =>
-                            Future.successful(Redirect(routes.ThereIsAProblemController.onPageLoad()))
-                        }
-                    }
-                }
-              } else {
-                subscriptionService.getContactDetails(ua, request.subscriptionId) flatMap {
-                  case Some(userAnswers) =>
-                    sessionRepository.set(userAnswers) flatMap {
-                      _ =>
-                        if (userAnswers.get(ContactNamePage).isEmpty) {
-                          Future.successful(Redirect(routes.ContactDetailsNeededController.onPageLoad()))
-                        } else {
-                          fileConnector.getAllFileDetails(request.subscriptionId) map {
-                            fileDetails =>
-                              Ok(view(fileDetails.isDefined, request.subscriptionId, request.isAgent))
-                          }
-                        }
-                    }
-                  case _ =>
-                    Future.successful(Redirect(routes.ThereIsAProblemController.onPageLoad()))
-                }
-              }
+          if (request.isAgent) {
+            checkForAgentContactDetails(request.subscriptionId, ua)
+          } else {
+            checkForOrgContactDetails(request.subscriptionId, ua)
           }
       }
   }
+
+
+  private def checkForAgentContactDetails(subscriptionId: String, ua: UserAnswers)(implicit hc: HeaderCarrier, request: Request[_]): Future[Result] = {
+    agentSubscriptionService.getAgentContactDetails(ua) flatMap {
+      agentContactDetails =>
+        subscriptionService.getContactDetails(agentContactDetails.getOrElse(ua), subscriptionId) flatMap {
+          clientContactDetails =>
+            (agentContactDetails, clientContactDetails) match {
+              case (Some(agentUserAnswers), Some(clientUserAnswers)) =>
+                sessionRepository.set(agentUserAnswers).flatMap {
+                  _ =>
+                    sessionRepository.set(clientUserAnswers).flatMap {
+                      _ =>
+                        if (agentUserAnswers.get(AgentFirstContactNamePage).isEmpty) {
+                          Future.successful(Redirect(controllers.agent.routes.AgentContactDetailsNeededController.onPageLoad()))
+                        } else if (clientUserAnswers.get(ContactNamePage).isEmpty) {
+                          Future.successful(Redirect(controllers.client.routes.ClientContactDetailsNeededController.onPageLoad()))
+                        } else {
+                          fileConnector.getAllFileDetails(subscriptionId) map {
+                            fileDetails =>
+                              Ok(view(fileDetails.isDefined, subscriptionId, isAgent = true))
+                          }
+                        }
+                    }
+                }
+              case _ =>
+                Future.successful(Redirect(routes.ThereIsAProblemController.onPageLoad()))
+            }
+        }
+    }
+  }
+
+  private def checkForOrgContactDetails(subscriptionId: String, ua: UserAnswers)(implicit hc: HeaderCarrier, request: Request[_]): Future[Result] = {
+    subscriptionService.getContactDetails(ua, subscriptionId) flatMap {
+      case Some(userAnswers) =>
+        sessionRepository.set(userAnswers) flatMap {
+          _ =>
+            if (userAnswers.get(ContactNamePage).isEmpty) {
+              Future.successful(Redirect(routes.ContactDetailsNeededController.onPageLoad()))
+            } else {
+              fileConnector.getAllFileDetails(subscriptionId) map {
+                fileDetails =>
+                  Ok(view(fileDetails.isDefined, subscriptionId, isAgent = false))
+              }
+            }
+        }
+      case _ =>
+        Future.successful(Redirect(routes.ThereIsAProblemController.onPageLoad()))
+    }
+  }
+
+  private def setContactDetailsFlag(userAnswers: UserAnswers): Future[UserAnswers] =
+    Future.fromTry(userAnswers.set(JourneyInProgressPage, true)).flatMap {
+      ua =>
+        sessionRepository.set(ua).map {
+          _ => ua
+        }
+    }
 }
