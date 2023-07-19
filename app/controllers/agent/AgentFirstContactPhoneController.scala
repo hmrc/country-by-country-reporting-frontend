c72955bc61c755032f189bb895056dc85b97cde1
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2023-07-19 09:56:51 +0100

    Added changes to agent contact details page

--- app/controllers/agent/AgentFirstContactPhoneController.scala
+++ app/controllers/agent/AgentFirstContactPhoneController.scala
@@ -55,15 +55,15 @@ class AgentFirstContactPhoneController @Inject() (
         case Some(value) => form.fill(value)
       }
 
-      Ok(view(preparedForm, getPluralAgentFirstContactName(request.userAnswers), mode))
+      Ok(view(preparedForm, getAgentFirstContactName(request.userAnswers), mode))
   }
-
+§
   def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData() andThen requireData).async {
     implicit request =>
       form
         .bindFromRequest()
         .fold(
-          formWithErrors => Future.successful(BadRequest(view(formWithErrors, getPluralAgentFirstContactName(request.userAnswers), mode))),
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, getAgentFirstContactName(request.userAnswers), mode))),
           value =>
             for {
               updatedAnswers <- Future.fromTry(request.userAnswers.set(AgentFirstContactPhonePage, value))
