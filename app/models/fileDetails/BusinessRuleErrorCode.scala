c8c98d7fff4ef953e3367eecccc36ad677a4971e
Carl Thompson <carl.thompson@opencastsoftware.com>
2024-01-23 15:25:52 +0000

    content updates to error messages

--- app/models/fileDetails/BusinessRuleErrorCode.scala
+++ app/models/fileDetails/BusinessRuleErrorCode.scala
@@ -16,6 +16,7 @@
 
 package models.fileDetails
 
+import models.fileDetails.BusinessRuleErrorCode.CorrMessageRefIDForbiddenInMessageHeader
 import play.api.libs.json.{__, JsString, Reads, Writes}
 
 sealed abstract class BusinessRuleErrorCode(val code: String)
@@ -23,73 +24,144 @@ sealed abstract class BusinessRuleErrorCode(val code: String)
 object BusinessRuleErrorCode {
 
   case object InvalidMessageRefIDFormat extends BusinessRuleErrorCode("50008")
+
   case object MessageRefIDHasAlreadyBeenUsed extends BusinessRuleErrorCode("50009")
+
   case object FileContainsTestDataForProductionEnvironment extends BusinessRuleErrorCode("50010")
+
   case object NotMeantToBeReceivedByTheIndicatedJurisdiction extends BusinessRuleErrorCode("50012")
+
   case object DocRefIDAlreadyUsed extends BusinessRuleErrorCode("80000")
+
   case object DocRefIDFormat extends BusinessRuleErrorCode("80001")
+
   case object CorrDocRefIdUnknown extends BusinessRuleErrorCode("80002")
+
   case object CorrDocRefIdNoLongerValid extends BusinessRuleErrorCode("80003")
+
   case object CorrDocRefIdForNewData extends BusinessRuleErrorCode("80004")
+
   case object MissingCorrDocRefId extends BusinessRuleErrorCode("80005")
+
+  case object CorrMessageRefIDForbiddenInDocSpec extends BusinessRuleErrorCode("80006")
+
+  case object CorrMessageRefIDForbiddenInMessageHeader extends BusinessRuleErrorCode("80007")
+
   case object ResendOption extends BusinessRuleErrorCode("80008")
+
   case object DeleteParentRecord extends BusinessRuleErrorCode("80009")
+
   case object MessageTypeIndic extends BusinessRuleErrorCode("80010")
+
   case object CorrDocRefIDTwiceInSameMessage extends BusinessRuleErrorCode("80011")
+
   case object UnknownDocRefID extends BusinessRuleErrorCode("80013")
+
   case object DocRefIDIsNoLongerValid extends BusinessRuleErrorCode("80014")
 
   case object MoreThanOneCBCBody extends BusinessRuleErrorCode("1")
+
   case object MessageSpecLargerThanSendEntity extends BusinessRuleErrorCode("2")
+
   case object DocRefIDMustMatchMessageRefID extends BusinessRuleErrorCode("3")
+
   case object MessageRefIDFileNameMatch extends BusinessRuleErrorCode("4")
+
   case object ReportingPeriodMatchYear extends BusinessRuleErrorCode("5")
+
   case object CBCIDIncorrect extends BusinessRuleErrorCode("6")
+
   case object MessageTypeIndicMatch extends BusinessRuleErrorCode("7")
+
   case object XMLTimestampMatch extends BusinessRuleErrorCode("8")
+
   case object TINMatch extends BusinessRuleErrorCode("9")
+
   case object DocTypeIndicMatch extends BusinessRuleErrorCode("10")
+
   case object XMLParentGroupMatch extends BusinessRuleErrorCode("11")
+
   case object ReportingPeriodCantChange extends BusinessRuleErrorCode("12")
+
   case object ReportingPeriodMatchEndDate extends BusinessRuleErrorCode("13")
+
   case object MultipleInitialReportingPeriods extends BusinessRuleErrorCode("14")
+
   case object PreviousSubmissionReportingTypeOverlap extends BusinessRuleErrorCode("15a")
+
   case object PreviousSubmissionReportingTypeOverlap2 extends BusinessRuleErrorCode("15b")
+
   case object ReportingPeriodStartBeforeEnd extends BusinessRuleErrorCode("16")
+
   case object OldestAllowableReportingPeriod extends BusinessRuleErrorCode("17")
+
   case object FutureReportingPeriod extends BusinessRuleErrorCode("18")
+
   case object ReportingEntityTINFormat extends BusinessRuleErrorCode("20")
+
   case object ConstEntityMatchInitial extends BusinessRuleErrorCode("21a")
+
   case object ConstEntityMatchCorrection extends BusinessRuleErrorCode("21b")
+
   case object DuplicationOfConstEntity extends BusinessRuleErrorCode("22a")
+
   case object DuplicationOfConstEntity2 extends BusinessRuleErrorCode("22b")
+
   case object CurrencyCodeInitial extends BusinessRuleErrorCode("23a")
+
   case object CurrencyCodeCorrection extends BusinessRuleErrorCode("23b")
+
   case object ConstEntitiesRoleInitial extends BusinessRuleErrorCode("24a")
+
   case object ConstEntitiesRoleCorrection extends BusinessRuleErrorCode("24b")
+
   case object ReportingRoleIsCBC701Initial extends BusinessRuleErrorCode("25a")
+
   case object ReportingRoleIsCBC701Correction extends BusinessRuleErrorCode("25b")
+
   case object ReportingRoleIsCBC702Initial extends BusinessRuleErrorCode("26a")
+
   case object ReportingRoleIsCBC702Correction extends BusinessRuleErrorCode("26b")
+
   case object ReportingRoleIsCBC702Initial2 extends BusinessRuleErrorCode("27a")
+
   case object ReportingRoleIsCBC702Correction2 extends BusinessRuleErrorCode("27b")
+
   case object WhiteSpaceSchemaValidation extends BusinessRuleErrorCode("28")
+
   case object BizActivitiesCBC513 extends BusinessRuleErrorCode("29")
+
   case object NegativeNumberOfEmployees extends BusinessRuleErrorCode("30")
+
   case object Exceeding10mEmployees extends BusinessRuleErrorCode("31")
+
   case object InitialMustContainOneCBCReport extends BusinessRuleErrorCode("32")
+
   case object OECD0CorrDocRefMustNotBePresent extends BusinessRuleErrorCode("33")
+
   case object MessageTypeIndicCBC402andOECD0 extends BusinessRuleErrorCode("34")
+
   case object MessageTypeIndicCBC401DocTypeOnlyOECD1 extends BusinessRuleErrorCode("35")
+
   case object SchemaVersionNumber extends BusinessRuleErrorCode("36")
+
   case object TINProvidedIssuedByAttributeMustBeCompleted extends BusinessRuleErrorCode("37")
+
   case object SendingEntityINIsAMandatoryField extends BusinessRuleErrorCode("38")
+
   case object SpecificReportingRoleCantMatchSpecificConstEntityRole extends BusinessRuleErrorCode("39a")
+
   case object SpecificReportingRoleCantMatchSpecificConstEntityRole2 extends BusinessRuleErrorCode("39b")
+
   case object SpecificReportingRoleCantMatchSpecificConstEntityRole3 extends BusinessRuleErrorCode("40a")
+
   case object SpecificReportingRoleCantMatchSpecificConstEntityRole4 extends BusinessRuleErrorCode("40b")
+
   case object ReportingRoleOECD0IsNotTheSame extends BusinessRuleErrorCode("42")
+
   case object ReportingRoleOECD0IsNotTheSame2 extends BusinessRuleErrorCode("43")
+
+  case object CustomError extends BusinessRuleErrorCode("99999")
 
   case class UnknownErrorCode(override val code: String) extends BusinessRuleErrorCode(code)
 
@@ -108,6 +180,8 @@ object BusinessRuleErrorCode {
     CorrDocRefIdNoLongerValid,
     CorrDocRefIdForNewData,
     MissingCorrDocRefId,
+    CorrMessageRefIDForbiddenInDocSpec,
+    CorrMessageRefIDForbiddenInMessageHeader,
     ResendOption,
     DeleteParentRecord,
     MessageTypeIndic,
@@ -164,7 +238,8 @@ object BusinessRuleErrorCode {
     SpecificReportingRoleCantMatchSpecificConstEntityRole3,
     SpecificReportingRoleCantMatchSpecificConstEntityRole4,
     ReportingRoleOECD0IsNotTheSame,
-    ReportingRoleOECD0IsNotTheSame2
+    ReportingRoleOECD0IsNotTheSame2,
+    CustomError
   )
 
   implicit val writes: Writes[BusinessRuleErrorCode] = Writes[BusinessRuleErrorCode] {
@@ -183,6 +258,8 @@ object BusinessRuleErrorCode {
     case "80003"           => CorrDocRefIdNoLongerValid
     case "80004"           => CorrDocRefIdForNewData
     case "80005"           => MissingCorrDocRefId
+    case "80006"           => CorrMessageRefIDForbiddenInDocSpec
+    case "80007"           => CorrMessageRefIDForbiddenInMessageHeader
     case "80008"           => ResendOption
     case "80009"           => DeleteParentRecord
     case "80010"           => MessageTypeIndic
@@ -240,6 +317,7 @@ object BusinessRuleErrorCode {
     case "CBCErrorCode40b" => SpecificReportingRoleCantMatchSpecificConstEntityRole4
     case "CBCErrorCode42"  => ReportingRoleOECD0IsNotTheSame
     case "CBCErrorCode43"  => ReportingRoleOECD0IsNotTheSame2
+    case "99999"           => CustomError
     case otherCode         => UnknownErrorCode(otherCode)
   }
 }
