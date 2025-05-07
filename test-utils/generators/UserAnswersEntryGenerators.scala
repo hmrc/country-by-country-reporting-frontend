0edba9876f60c65de9991da9905b40d4b1b0b367
Carl Thompson <carl.thompson@opencastsoftware.com>
2025-05-07 08:39:37 +0100

    fix another cyclic inheritance

--- test-utils/generators/UserAnswersEntryGenerators.scala
+++ test-utils/generators/UserAnswersEntryGenerators.scala
@@ -22,7 +22,7 @@ import org.scalacheck.Arbitrary.arbitrary
 import pages._
 import play.api.libs.json.{JsValue, Json}
 
-trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators with Generators {
+trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {
 
   implicit lazy val arbitraryReviewClientContactDetailsUserAnswersEntry: Arbitrary[(ReviewClientContactDetailsPage.type, JsValue)] =
     Arbitrary {
