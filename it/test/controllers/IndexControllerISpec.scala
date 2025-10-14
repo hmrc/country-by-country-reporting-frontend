1c874ef8233a7a70a60de81abc279367b7e64a29
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2025-10-14 04:06:58 +0100

    DAC6-3344 | fix url assert in integration test

--- it/test/controllers/IndexControllerISpec.scala
+++ it/test/controllers/IndexControllerISpec.scala
@@ -90,7 +90,7 @@ class IndexControllerISpec extends PlaySpec with ISpecBase {
           .get()
       )
       response.status mustBe SEE_OTHER
-      response.header("Location") mustBe Some("/send-a-country-by-country-report/unauthorised")
+      response.header("Location") mustBe Some("/send-a-country-by-country-report/problem/unauthorised")
       verifyPost(authUrl)
     }
   }
